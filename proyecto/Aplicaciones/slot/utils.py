baee0705533ce183e0ea0821c686c138070e0e50
Rabana21 <rbnbullones1@uma.es>
2022-07-18 11:58:31 +0200

    Algoritmo_asignación v_0,1

--- proyecto/Aplicaciones/slot/utils.py
+++ proyecto/Aplicaciones/slot/utils.py
@@ -8,7 +8,7 @@ from Aplicaciones.request_class.models import RequestClass
 from .models import Slot
 
 COMFLICTS = []
-
+MAX_STRIKES = 3
 
 def unique_codes():
     codes = RequestClass.objects.values_list('code')
@@ -35,19 +35,30 @@ def real_targets(request):
     
 def matches():
     unique_codes = unique_codes() # Conjunto candidatos
-    match = {} # Diccionario con una asignación de clase con las peticiones que puede satisfacer
-    while unique_codes != None:
-        q = RequestClass.objects.filter(code=unique_codes[0])
+    matches = [] # Diccionario con una asignación de clase con las peticiones que puede satisfacer
+    for code in unique_codes:
+        q = RequestClass.objects.filter(code=code)
         for r in q.iterator():
             real_targets = real_targets(r) # Lista de clases con las mismas características
-            match[r] = real_targets
+            match = {}
+            match[r.code] = real_targets
+            matches.append(match)
+            break
 
 def preprocess():
     matches = matches()
 
+    # Ordenar de menos posibilidades para elegir a más posibilidades para elegir
+    sorted_matches = sorted(matches, key=lambda x:x[1])
+    solution = {}
+    for k in unique_codes:
+        solution[k] = None
+
+    strikes = 0
+
+    assign(solution, sorted_matches, strikes)
+
     # COSAS QUE HACER
-    # 1. ORDENAR EL DICCIONARIO DE MATCHES POR EL NUMERO DE POSIBLES AULAS,
-    # PRIMERO LAS QUE TIENEN MENOS AULAS POSIBLES, DESPUES LAS QUE MAS
     # 2. LAS FRANJAS HORARIAS DE LOS SLOT SON 6: 1,2,3 MAÑANA Y 4,5,6 TARDE
     # 3. LOS EXAMENES/MASTER (CREO QUE LOS MASTER TAMBIEN) OCUPAN DOS FRANJAS
     # ASI QUE NO PUEDEN RESERVAR LAS FRANJAS 3 NI 6
@@ -58,4 +69,63 @@ def preprocess():
     # SUMA 1 CONFLICTO Y SE SIGUE, ASIGNANDO AHORA LA PET3}
 
 
+def assign(solution, matches, strikes, preference, count, classroom):
+    # solution-> {1125:{"franja":1,"clase":"lab_01"},...}
+    # matches-> [1125:["lab_01", "lab_02", "lab_03",...], 1126:["lab_02"], ...]
+    # strikes-> nº de conflictos en la solucion
+    # preference-> "Preferente" o "Alternativo"
+    # count-> Posición en la lista de matches que habrá que hacer la elección
+    # classroom-> Classroom.class
 
+    target = matches[count] # Codigo de la reserva para la siguiente elección
+    request = RequestClass.objects.get(code = target.keys()[0], preference=preference)
+    solution, conflict = add_choice(solution, matches, request, classroom)
+    
+    is_empty = [{x:v} for x,v in solution.items() if v==None]
+    if len(is_empty) == 0:
+        # Ya se han asignado todas las peticiones
+        return strikes, solution
+
+    if strikes == MAX_STRIKES:
+        return None, solution
+    if conflict:
+        strikes += 1
+
+    q = RequestClass.objects.filter(code__in = target.keys())
+    choice_preferente = None
+    choice_alternativo = None
+    for r in q.iterator():
+        if r.preference =='Preferente':
+            choice_preferente = r
+        else:
+            choice_alternativo = r
+
+    min_strikes_preferente = float('inf')
+    min_strikes_alternativo = float('inf')
+    preferente = {}
+    alternativo = {}
+    for _,classroom in matches[count].items():
+        strikes_preferente, preferente = assign(solution, matches, strikes, "Preferente", count+1, classroom)
+        if strikes_preferente < min_strikes_preferente:
+            preferente = preferente
+        strikes_alternativo, alternativo = assign(solution, matches, strikes, "Alternativo", count+1, classroom)
+        if strikes_alternativo < min_strikes_alternativo:
+            alternativo = alternativo
+    min_strikes = min(strikes_preferente, strikes_alternativo)
+    if min_strikes == strikes_preferente:
+        return strikes_preferente, preferente
+    else:
+        return strikes_alternativo, alternativo
+
+
+def add_choice(solution, matches, request, classroom) -> tuple (dict, bool):
+    # Añadir la elección a la solución, donde en solución tendrá que estar como clave 
+    # el código de la reserva y como valor -> franja:1 y si es una franja mas larga por 
+    # examen poner -> franja:[1,2]. Tener en cuenta que si se reserva dos franjas no puedas reservar la [3,4]
+    # y por ultimo en la elección seleccionar la clase
+    None
+
+def conflict(solution, classroom, schedule) -> bool:
+    # Comprobar en la solucion si hay conflictos entre la shcedule y la clase elegida entre las elecciones
+    # de la solucion
+    None 
